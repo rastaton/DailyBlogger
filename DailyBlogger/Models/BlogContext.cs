b877fb3b95baf5e1e62aeddab4d3face334e3735
Richard Staton <rncstaton@hotmail.com>
2020-08-04 10:26:57 -0500

    Add project files.

---
+++ DailyBlogger/Models/BlogContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyBlogger.Models
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<BlogPost> blogPosts { get; set; }
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+
+        }
+    }
+}
